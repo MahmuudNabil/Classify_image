7b83e5187e621252003beb50ff6b7cec06fa0259
mahmoud <m.n.hakiem17@gmail.com>
2020-01-26 09:10:27 +0200

    second commit

---
+++ src/classifyimages/ClassifyImages.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classifyimages;
+
+/**
+ *
+ * @author Mahmoud
+ */
+public class ClassifyImages {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      MainFrame m = new MainFrame();
+      m.setVisible(true);
+      m.setLocationRelativeTo(null);
+      m.setDefaultCloseOperation(1);
+    }
+    
+}
